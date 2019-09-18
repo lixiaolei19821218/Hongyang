53a5309942f35e57bebeefda6bde45907590a8b5
lixiaolei19821218 <735534185@qq.com>
2019-09-18 16:52:31 +0800

    add

--- Hongyang/MainWindow.xaml.cs
+++ Hongyang/MainWindow.xaml.cs
@@ -37,14 +37,20 @@ namespace Hongyang
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //powerMILL.DialogsOff();
-            powerMILL.Execute("MODE WORKPLANE_CREATE ; INTERACTIVE GEOMETRY");
-            powerMILL.Execute("PICK -266.521 -149.743 266.521 149.743 -875.776 -386.645 488.954 -0.994872 -0.0357478 0.0946113 0.0962291 -0.0466247 0.994267 0 -21.046 -21.2323 -21.046 -21.2323");
+            WindowState = WindowState.Minimized;
+            powerMILL.DialogsOff();            
+            //powerMILL.Execute("MODE WORKPLANE_CREATE ; INTERACTIVE GEOMETRY");           
+            //powerMILL.Execute("PICK -266.521 -149.743 266.521 149.743 -875.776 -386.645 488.954 -0.994872 -0.0357478 0.0946113 0.0962291 -0.0466247 0.994267 0 -21.046 -21.2323 -21.046 -21.2323");
+            powerMILL.Execute($"EDIT LEVEL \"{cbxLevel.Text}\" SELECT ALL");
+            powerMILL.Execute("MODE WORKPLANE_CREATE ; SELECTION CENTRE");
             session.Refresh();
             PMWorkplane workplane = session.Workplanes.Last();
+            //workplane.ToWorkplane().
+            //workplane.ToWorkplane().
             powerMILL.Execute($"EXPLORER SELECT Workplane \"Workplane\\{workplane.Name}\" NEW");
             powerMILL.Execute($"ACTIVATE Workplane \"{workplane.Name}\"");            
             powerMILL.Execute("CREATE TOOL ; PROBE FORM TOOL");
+            //powerMILL.Execute("CREATE TOOL");
             session.Refresh();
             PMTool tool = session.Tools.Last();
             powerMILL.Execute($"EDIT TOOL \"{tool.Name}\" DIAMETER \"{tbxDiameter.Text}\"");
@@ -57,7 +63,7 @@ namespace Hongyang
             powerMILL.Execute($"EDIT TOOL \"{tool.Name}\" HOLDER_COMPONENT LENGTH \"30\"");
             powerMILL.Execute($"EDIT TOOL \"{tool.Name}\" OVERHANG \"{tbxOverhang.Text}\"");
             powerMILL.Execute("TOOL ACCEPT");
-            powerMILL.Execute("FORM STRATEGYSELECTOR");
+            //powerMILL.Execute("FORM STRATEGYSELECTOR");
             powerMILL.Execute("STRATEGYSELECTOR CATEGORY 'Probing' NEW");
             powerMILL.Execute("STRATEGYSELECTOR STRATEGY \"Probing/Surface-Inspection.ptf\" NEW");
             powerMILL.Execute("IMPORT TEMPLATE ENTITY TOOLPATH TMPLTSELECTORGUI \"Probing/Surface-Inspection.ptf\"");
@@ -82,11 +88,15 @@ namespace Hongyang
             session.Refresh();
             PMPattern pattern2 = session.Patterns.Last();
             powerMILL.Execute($"EDIT PATTERN \"{pattern2.Name}\" INSERT MODEL");
+            //powerMILL.Execute("VIEW MODEL ; SHADE OFF");
+            //powerMILL.Execute("PICK -108.57 -60.9996 108.57 60.9996 -746.448 -671.323 259.199 -0.77469 -0.551684 -0.30903 0.00239525 -0.491265 0.871007 0 14.3395 -21.6989 17.526 -29.2859");
+            //powerMILL.Execute($"EDIT LEVEL \"{cbxLevel.Text}\" SELECT ALL");
+            //powerMILL.Execute("DELETE SELECTION");
+            powerMILL.Execute($"EDIT LEVEL \"{cbxLevel.Text}\" SELECT ALL");
             powerMILL.Execute("VIEW MODEL ; SHADE OFF");
-            powerMILL.Execute("PICK -108.57 -60.9996 108.57 60.9996 -746.448 -671.323 259.199 -0.77469 -0.551684 -0.30903 0.00239525 -0.491265 0.871007 0 14.3395 -21.6989 17.526 -29.2859");
-            powerMILL.Execute("DELETE SELECTION");
-            powerMILL.Execute("VIEW MODEL ; SHADE NORMAL");
-            powerMILL.Execute("PICK -108.57 -60.9996 108.57 60.9996 -746.448 -671.323 259.199 -0.77469 -0.551684 -0.30903 0.00239525 -0.491265 0.871007 0 -61.8342 -13.0497 -61.8342 -13.0497");
+            //powerMILL.Execute("PICK -108.57 -60.9996 108.57 60.9996 -746.448 -671.323 259.199 -0.77469 -0.551684 -0.30903 0.00239525 -0.491265 0.871007 0 -61.8342 -13.0497 -61.8342 -13.0497");
+            powerMILL.Execute($"EDIT PATTERN \"{pattern2.Name}\" DESELECT 1");
+            powerMILL.Execute($"EDIT PATTERN \"{pattern2.Name}\" SELECT 0");            
             powerMILL.Execute($"EDIT PATTERN \"{pattern2.Name}\" CURVEEDITOR START");
             powerMILL.Execute("FORM RIBBON TAB \"CurveTools.EditCurve\"");
             powerMILL.Execute("CURVEEDITOR REPOINT RAISE");
@@ -109,7 +119,7 @@ namespace Hongyang
             powerMILL.Execute("VIEW MODEL ; SHADE NORMAL");
             powerMILL.Execute("VIEW MODEL ; SHADE OFF");
             powerMILL.Execute("FORM RIBBON TAB \"CurveEditor.Edit\"");
-            powerMILL.Execute("PICK -153.567 -86.2806 153.567 86.2806 -694.01 -774.64 376.336 -0.716069 -0.691892 -0.0923641 -0.0390162 -0.0924418 0.994953 0 -119.441 1.5024 -117.724 -38.2039");
+            //powerMILL.Execute("PICK -153.567 -86.2806 153.567 86.2806 -694.01 -774.64 376.336 -0.716069 -0.691892 -0.0923641 -0.0390162 -0.0924418 0.994953 0 -119.441 1.5024 -117.724 -38.2039");
             powerMILL.Execute("FORM RIBBON TAB \"CurveTools.EditCurve\"");
             powerMILL.Execute("CURVEEDITOR FILLET INSERT RAISE");
             powerMILL.Execute("FORM ACCEPT CEINSERTFILLET");
@@ -125,13 +135,14 @@ namespace Hongyang
             powerMILL.Execute($"EDIT TOOLPATH \"{clone.Name}\" CALCULATE");
             powerMILL.Execute("FORM ACCEPT SFSurfaceInspect");
             powerMILL.Execute("VIEW MODEL ; SHADE NORMAL");
-            powerMILL.Execute("PICK -86.3812 -48.5329 86.3812 48.5329 -743.703 -699.275 320.499 -0.748899 -0.632926 -0.196355 -0.140636 -0.137758 0.980431 0 -73.8255 9.53755 -62.477 -18.23");
+            //powerMILL.Execute("PICK -86.3812 -48.5329 86.3812 48.5329 -743.703 -699.275 320.499 -0.748899 -0.632926 -0.196355 -0.140636 -0.137758 0.980431 0 -73.8255 9.53755 -62.477 -18.23");
             powerMILL.Execute("FORM RIBBON TAB \"ToolpathEdit\"");
             powerMILL.Execute("DELETE TOOLPATH ; SELECTED");
             powerMILL.Execute("PICK -86.3812 -48.5329 86.3812 48.5329 -743.703 -699.275 320.499 -0.748899 -0.632926 -0.196355 -0.140636 -0.137758 0.980431 0 34.7094 8.08881 45.2128 -23.9042");
-            powerMILL.Execute("DELETE TOOLPATH ; SELECTED");
+            //powerMILL.Execute("DELETE TOOLPATH ; SELECTED");
 
-            MessageBox.Show("计算完成");
+            WindowState = WindowState.Normal;
+            MessageBox.Show("计算完成", "Info", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
         }
     }
 }
